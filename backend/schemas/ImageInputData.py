40678419b1cb134498bca150a6ecf183c274b3a2
Siddhesh Agarwal <siddhesh.agarwal@gmail.com>
2023-08-03 09:54:23 +0530

    strongly typed the schemas

--- backend/schemas/ImageInputData.py
+++ backend/schemas/ImageInputData.py
@@ -1,5 +1,3 @@
-from typing import Optional
-
 from pydantic import AnyHttpUrl, BaseModel, validator
 
 
@@ -8,9 +6,9 @@ class ImageInputData(BaseModel):
     picture_url: AnyHttpUrl
 
     @validator("picture_url")
-    def check_image_type(cls, v):
-        v = v.split("?", 1)[0]
-        allowed_file_extensions = [".jpg", ".png", ".jpeg"]
-        if any(v.endswith(extension) for extension in allowed_file_extensions):
+    def check_image_type(cls, v: AnyHttpUrl) -> AnyHttpUrl:
+        str_v: str = v.split("?", 1)[0]
+        allowed_file_extensions: list[str] = [".jpg", ".png", ".jpeg"]
+        if any(str_v.endswith(extension) for extension in allowed_file_extensions):
             return v
         raise Exception("Unsupported file extension. This file extension is not yet supported.")
--- backend/schemas/TextInputData.py
+++ backend/schemas/TextInputData.py
@@ -6,14 +6,14 @@ class TextInputData(BaseModel):
     content: str
 
     @validator("content")
-    def clean_summary(cls, v):
+    def clean_summary(cls, v: str) -> str:
         v = v.strip()
         v = v.replace("\n", " ")
         v = v.replace("\t", " ")
         return v.lower()
 
     class Config:
-        schema_extra = {
+        schema_extra: dict[str, dict[str, str]] = {
             "example": {
                 "url": "https://www.google.com",
                 "content": "This is a test",
