37e47905362c2ed516a41923c93c28ea61947beb
Siddhesh Agarwal <siddhesh.agarwal@gmail.com>
2023-08-03 18:22:21 +0530

    removed extra imports

--- backend/core/__init__.py
+++ backend/core/__init__.py
@@ -1,3 +1,2 @@
-from core.db import add_to_db, fetch_from_db_if_exists  # type: ignore
-from core.fact import fact_check_process, fact_check_this  # type: ignore
+from core.fact import fact_check_process  # type: ignore
 from core.utils import get_image, summarize, to_english  # type: ignore
