592ac9a5c7f4bd5fd739601078e41004c72a6fa6
Siddhesh Agarwal <siddhesh.agarwal@gmail.com>
2023-08-04 18:35:17 +0530

    styled with TailwindCSS

--- frontend/popup.js
+++ frontend/popup.js
@@ -1,3 +1,15 @@
+// type Message = {
+//     url: string;
+//     archive: string;
+//     summary: string;
+//     response: string;
+//     label: Boolean;
+//     confidence: number;
+//     references: string[];
+//     isCredible: Boolean;
+//     isPhishing: Boolean;
+// }
+
 // Display the response to the popup window
 chrome.runtime.onMessage.addListener(
     function (message, sender, sendResponse) {
@@ -12,9 +24,48 @@ chrome.runtime.onMessage.addListener(
 
             console.log(message.data.references);
 
-            let links = message.data.references.map((ref, index) => `${index + 1}) <a href="${ref}">${ref}</a>`);
             let data = message.data;
-            let result_display = `<b>State of News :</b><br> ${data.label}<br><b>Response :</b><br> ${data.response}<br><b>Archived URL: </b>${data.archive}<br><b>Confidence :</b><br> ${data.confidence}<br><b>References :</b><br>${links.join("<br>")}<br><b>Credible:</b> ${data.isCredible}`;
+            // Use Tailwind CSS to style the popup
+            let result_display = `
+            <div class="text-2xl font-bold mb-2">
+                <b style="color: #3B82F6;">
+                    State of News:
+                </b>
+                <span> ${data.label}</span>
+            </div>
+            <div class="text-xl font-bold mb-2">
+                <b style="color: #3B82F6;">
+                    Response:
+                </b>
+                <span> ${data.response}</span>
+            </div>
+            <div class="text-xl font-bold mb-2">
+                <b style="color: #3B82F6;">
+                    Archived URL:
+                </b>
+                <span>${data.archive}</span>
+            </div>
+            <div class="text-xl font-bold mb-2">
+                <b style="color: #3B82F6;">
+                    Confidence:
+                </b>
+                <span> ${data.confidence}</span>
+            </div>
+            <div class="text-xl font-bold mb-2">
+                <b style="color: #3B82F6;">
+                    References:
+                </b>
+                <ul class="list-disc list-inside">
+                ${data.references.map((ref) => `<li><a href="${ref}">${ref}</a></li>`).join()}
+                </ul>
+            </div>
+            <div class="text-xl font-bold mb-2">
+                <b style="color: #3B82F6;">
+                    Credible:
+                </b>
+                <span>${data.isCredible}</span>
+            </div>
+            `;
             ele.innerHTML = result_display;
         }
     }
