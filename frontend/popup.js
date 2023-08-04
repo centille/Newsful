2f7bedd9f2a8a970d62a8cfc1af0af67d9a94d87
Siddhesh Agarwal <siddhesh.agarwal@gmail.com>
2023-08-04 19:55:51 +0530

    fixed frontend style

--- frontend/popup.js
+++ frontend/popup.js
@@ -15,55 +15,71 @@ chrome.runtime.onMessage.addListener(
     function (message, sender, sendResponse) {
         if (message.action === "displayResponse") {
             const loading = document.getElementById("loading");
-
             if (loading && loading.parentNode) {
                 loading.parentNode.removeChild(loading);
             }
 
             let ele = document.getElementById("result");
-
-            console.log(message.data.references);
-
             let data = message.data;
+            console.log(data.references);
+
             // Use Tailwind CSS to style the popup
             let result_display = `
-            <div class="text-2xl font-bold mb-2">
-                <b style="color: #3B82F6;">
-                    State of News:
-                </b>
-                <span> ${data.label}</span>
+            <div class="flex my-4">
+                <div class="text-md font-bold w-1/2 block mx-1">
+                    <div class="bg-blue-400 w-full text-center py-2 text-white rounded">
+                        State of News
+                    </div>
+                    <div class="text-md px-2">
+                        ${data.label}
+                    </div>
+                </div>
+                <div class="text-md font-bold w-1/2 block mx-1">
+                    <div class="bg-blue-400 w-full text-center py-2 text-white rounded">
+                        Confidence
+                    </div>
+                    <div class="text-md px-2 flex-wrap">
+                        ${data.confidence}
+                    </div>
+                </div>
             </div>
-            <div class="text-xl font-bold mb-2">
-                <b style="color: #3B82F6;">
-                    Response:
-                </b>
-                <span> ${data.response}</span>
+            <div class="flex my-3">
+                <div class="text-md font-bold w-1/2 block mx-1">
+                    <div class="bg-blue-400 w-full text-center py-2 text-white rounded">
+                        Phishing
+                    </div>
+                    <div class="text-md px-2">
+                        ${data.isPhishing?"YES":"NO"}
+                    </div>
+                </div>
+                <div class="text-md font-bold w-1/2 block mx-1">
+                    <div class="bg-blue-400 w-full text-center py-2 text-white rounded">
+                        Credible
+                    </div>
+                    <div class="text-md px-2 flex-wrap">
+                        ${data.isCredible?"YES":"NO"}
+                    </div>
+                </div>
             </div>
-            <div class="text-xl font-bold mb-2">
-                <b style="color: #3B82F6;">
-                    Archived URL:
-                </b>
-                <span>${data.archive}</span>
+            <div class="text-md font-bold block mx-1">
+                <div class="bg-blue-400 w-full text-center py-2 text-white rounded">
+                    Archive URL
+                </div>
+                <div class="text-md px-2">
+                    ${data.archiveUrl}
+                </div>
             </div>
-            <div class="text-xl font-bold mb-2">
-                <b style="color: #3B82F6;">
-                    Confidence:
-                </b>
-                <span> ${data.confidence}</span>
-            </div>
-            <div class="text-xl font-bold mb-2">
-                <b style="color: #3B82F6;">
-                    References:
-                </b>
-                <ul class="list-disc list-inside">
-                ${data.references.map((ref) => `<li><a href="${ref}">${ref}</a></li>`).join()}
-                </ul>
-            </div>
-            <div class="text-xl font-bold mb-2">
-                <b style="color: #3B82F6;">
-                    Credible:
-                </b>
-                <span>${data.isCredible}</span>
+            <div class="text-md font-bold block mx-1">
+                <div class="bg-blue-400 w-full text-center py-2 text-white rounded">
+                    References
+                </div>
+                <ol class="text-md px-2">
+                        ${
+                            data.references.map((ref)=>{
+                                return "<li class='text-md px-2'>" + ref + "</li>"
+                            })
+                        }
+                </ol>
             </div>
             `;
             ele.innerHTML = result_display;
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./index.html",
+        "./popup.html",
+        "./popup.js",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
+
